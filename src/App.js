46042177855eb818f83de686c9f93be4bc368d08
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2023-05-01 20:39:16 +0900

    feat: DiaryList 컴포넌트 생성 및 props 넘기기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
+
+const dummyList = [
+  { author: '조수진', content: '하이~', emotion: 2 },
+  { author: '조수진', content: '하이2~', emotion: 2 },
+  { author: '조수진', content: '하이3~', emotion: 2 },
+];
 
 function App() {
   return (
     <div className='App'>
       <h2>일기장</h2>
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
---
+++ src/DiaryList.js
@@ -0,0 +1,10 @@
+const DiaryList = ({ diaryList }) => {
+  console.log(diaryList);
+  return (
+    <div>
+      <h2>일기 리스트</h2>
+    </div>
+  );
+};
+
+export default DiaryList;
