ece52ac04c986d88d50f44cacf0218dfd06c1130
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2023-05-01 20:29:09 +0900

    feat: useRef로 DOM 조작

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,7 +1,10 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 const DiaryEditor = () => {
   const [state, setState] = useState({ author: '', content: '', emotion: 1 });
+
+  const authorInput = useRef();
+  const contentInput = useRef();
 
   const handleState = (e) => {
     setState({
@@ -11,7 +14,18 @@ const DiaryEditor = () => {
   };
 
   const handleClick = () => {
-    console.log(state);
+    if (state.author.length < 1) {
+      authorInput.current.focus();
+      // alert('작성자의 이름을 1자 이상 입력하세요');
+      return;
+    }
+
+    if (state.content.length < 5) {
+      contentInput.current.focus();
+      // alert('일기 내용을 5자 이상 입력하세요');
+      return;
+    }
+
     alert('저장 완료!');
   };
 
@@ -19,10 +33,20 @@ const DiaryEditor = () => {
     <div className='DiaryEditor'>
       <h2>오늘의 일기</h2>
       <div>
-        <input name='author' value={state.author} onChange={handleState} />
+        <input
+          ref={authorInput}
+          name='author'
+          value={state.author}
+          onChange={handleState}
+        />
       </div>
       <div>
-        <textarea name='content' value={state.content} onChange={handleState} />
+        <textarea
+          ref={contentInput}
+          name='content'
+          value={state.content}
+          onChange={handleState}
+        />
       </div>
       <div>
         <select name='emotion' value={state.emotion} onChange={handleState}>
