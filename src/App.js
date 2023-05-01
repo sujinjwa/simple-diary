cee7a0094a4a3aaba3816a5ea3380b0e57d48826
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2023-05-01 20:59:24 +0900

    feat: 일기 리스트 렌더링

--- src/App.js
+++ src/App.js
@@ -3,9 +3,27 @@ import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
 
 const dummyList = [
-  { author: '조수진', content: '하이~', emotion: 2 },
-  { author: '조수진', content: '하이2~', emotion: 2 },
-  { author: '조수진', content: '하이3~', emotion: 2 },
+  {
+    id: 1,
+    author: '조수진',
+    content: '하이~',
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: '조수진',
+    content: '하이2~',
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: '조수진',
+    content: '하이3~',
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
 ];
 
 function App() {
