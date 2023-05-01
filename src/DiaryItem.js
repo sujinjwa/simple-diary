0f7ab2899fd0253d5dedc0275aa6f5cdc1238c7e
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2023-05-01 21:20:06 +0900

    feat: DiaryItem 컴포넌트 생성

---
+++ src/DiaryItem.js
@@ -0,0 +1,16 @@
+const DiaryItem = ({ author, content, emotion, created_date, id }) => {
+  return (
+    <div className='DiaryItem'>
+      <div calssName='info'>
+        <span>
+          작성자 : {author} | 감정 점수: {emotion}
+        </span>
+        <br />
+        <span className='date'>{new Date(created_date).toLocaleString()}</span>
+      </div>
+      <div className='content'>{content}</div>
+    </div>
+  );
+};
+
+export default DiaryItem;
--- src/DiaryList.js
+++ src/DiaryList.js
@@ -1,18 +1,13 @@
+import DiaryItem from './DiaryItem';
+
 const DiaryList = ({ diaryList }) => {
   return (
-    <div>
+    <div className='DiaryList'>
       <h2>일기 리스트</h2>
       <h4>{diaryList.length}개의 일기가 있습니다.</h4>
       <div>
         {diaryList.map((elem) => {
-          return (
-            <div key={elem.id}>
-              <div>작성자: {elem.author}</div>
-              <div>일기: {elem.content}</div>
-              <div>감정: {elem.emotion}</div>
-              <div>시간: {elem.created_date}</div>
-            </div>
-          );
+          return <DiaryItem key={elem.id} {...elem} />;
         })}
       </div>
     </div>
