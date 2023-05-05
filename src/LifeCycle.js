d8ca2723d16a1b03d693971282eab548fa5cb3b1
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2023-05-06 00:11:53 +0900

    feat: 특정 컴포넌트의 마운트/언마운트 시점에서의 제어

--- src/LifeCycle.js
+++ src/LifeCycle.js
@@ -1,42 +1,27 @@
 import React, { useEffect, useState } from 'react';
 
-const Lifecycle = () => {
-  const [count, setCount] = useState(0);
-  const [text, setText] = useState('');
-
-  // mount될 때만 실행
+const UnmountTest = () => {
+  // UnmountTest 컴포넌트가 마운트될 때 실행
   useEffect(() => {
     console.log('Mount!');
+
+    // unmount 시점일 때 실행
+    return () => {
+      console.log('Unmount!');
+    };
   }, []);
 
-  // update될 때만 실행
-  useEffect(() => {
-    console.log('Update!');
-  });
+  return <div>Unmount Testing Component</div>;
+};
 
-  // count값이 udpate될 때만 실행
-  useEffect(() => {
-    console.log(`count is updated to: ${count}`);
-    if (count > 5) {
-      alert('count가 5를 초과했습니다. 0으로 초기화합니다.');
-      setCount(0);
-    }
-  }, [count]);
-
-  // text값이 update될 때만 실행
-  useEffect(() => {
-    console.log(`text is updated to : ${text}`);
-  }, [text]);
+const Lifecycle = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const toggle = () => setIsVisible(!isVisible);
 
   return (
     <div style={{ padding: 20 }}>
-      <div>
-        <span>{count}</span>
-        <button onClick={() => setCount(count + 1)}>+</button>
-      </div>
-      <div>
-        <input value={text} onChange={(e) => setText(e.target.value)} />
-      </div>
+      <button onClick={toggle}>ON/OFF</button>
+      {isVisible && <UnmountTest />}
     </div>
   );
 };
