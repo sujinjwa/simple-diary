9ae0f0faa42c36f904da6332daef2fafbac80845
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2023-05-06 00:56:16 +0900

    feat: fetch로 api 호출
    dataId를 useRef(0)으로 생성하고, datdaId.current++ 하면서 각 게시글의
    id값으로 설정

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
-import Lifecycle from './LifeCycle';
 
 function App() {
   const [data, setData] = useState([]); // data : 일기 리스트
 
+  const dataId = useRef(0);
+
+  // 프로미스를 반환하는 비동기 함수
+  const getData = async () => {
+    const res = await fetch(
+      'https://jsonplaceholder.typicode.com/comments'
+    ).then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((elem) => {
+      return {
+        id: dataId.current++,
+        author: elem.email,
+        content: elem.body,
+        emotion: Math.floor(Math.random() * 5) + 1, // 1 ~ 5 중 랜덤 생성
+        created_data: new Date().getTime(),
+      };
+    });
+
+    setData(initData);
+  };
+
+  useEffect(() => {
+    // app 컴포넌트가 mount되는 시점에 실행
+    getData();
+  }, []);
+
   const onCreate = (author, content, emotion, date) => {
+    dataId.current += 1;
     setData([
       ...data,
       {
-        id: data.length,
+        id: dataId.current,
         author: author,
         content: content,
         emotion: emotion,
@@ -37,7 +63,6 @@ function App() {
 
   return (
     <div className='App'>
-      <Lifecycle />
       <h2>일기장</h2>
       <DiaryEditor onCreate={onCreate} />
       <DiaryList diaryList={data} onRemove={onRemove} onEdit={onEdit} />
