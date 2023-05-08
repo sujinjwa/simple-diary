1f8583e5ee20776005d83e407507079aa4f0aad0
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2023-05-08 23:47:16 +0900

    feat: React.memo에게 두번째 인자로 비교 함수 제공
    props가 객체일 때 얕은 비교만 수행하는 것이 react의 기본 동작이다. 얕은
    비교가 아닌 다른 비교 동작을 원한다면, 두 번째 인자로 별도의 비교
    함수를  제공하면 된다(ex. 코드 상의 areEqual(prevProps, nextProps) 함수)

--- src/OptimizeTest.js
+++ src/OptimizeTest.js
@@ -1,37 +1,43 @@
 import React, { useState, useEffect } from 'react';
 
-const TextView = React.memo(({ text }) => {
+const CounterA = React.memo(({ count }) => {
   useEffect(() => {
-    console.log(`Update :: Text : ${text}`);
-  });
-  return <div>{text}</div>;
-});
-
-const CountView = React.memo(({ count }) => {
-  useEffect(() => {
-    console.log(`Update :: Count : ${count}`);
+    console.log(`CounterA Update :: count: ${count}`);
   });
   return <div>{count}</div>;
 });
 
+const CounterB = React.memo(({ obj }) => {
+  useEffect(() => {
+    console.log(`CounterB Update :: obj.count: ${obj.count}`);
+  });
+
+  return <div>{obj.count}</div>;
+});
+
+const areEqual = (prevProps, nextProps) => {
+  return prevProps.obj.count === nextProps.obj.count;
+};
+
+const MemoizedCounterB = React.memo(CounterB, areEqual);
+
 const OptimizeTest = () => {
   const [count, setCount] = useState(1);
-  const [text, setText] = useState('');
+  const [obj, setObj] = useState({
+    count: 1,
+  });
 
   return (
     <div style={{ padding: 50 }}>
       <div>
-        <h2>{count}</h2>
-        <CountView count={count} />
-        <button onClick={() => setCount(count + 1)}>+</button>
+        <h2>Counter A</h2>
+        <CounterA count={count} />
+        <button onClick={() => setCount(count)}>A Button</button>
       </div>
       <div>
-        <TextView text={text} />
-        <input
-          type='text'
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
+        <h2>Counter B</h2>
+        <MemoizedCounterB obj={obj} />
+        <button onClick={() => setObj({ count: obj.count })}>B Button</button>
       </div>
     </div>
   );
